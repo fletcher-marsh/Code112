966138d719f5f6b7df634bb1a0c583e2fbc33f4a
Fletcher Marsh <fletcher.r.marsh@gmail.com>
2019-04-15 21:37:56 -0400

    Interactive python shell working upon cmd (ctrl) + b

--- out/extension.js
+++ out/extension.js
@@ -1,19 +1,16 @@
 'use strict';
-import * as vscode from 'vscode';
-
+Object.defineProperty(exports, "__esModule", { value: true });
+const vscode = require("vscode");
 // Get current Python REPL (if exists)
-function getInteractivePythonTerminal(): vscode.Terminal | null {
+function getInteractivePythonTerminal() {
     const allTerms = vscode.window.terminals;
-
     allTerms.forEach(term => {
         if (term.name === `Python`) {
             return term;
         }
     });
-
     return null;
 }
-
 // Dispose existing Python REPL terminal (if exists)
 function disposePythonTerminal() {
     const curTerm = getInteractivePythonTerminal();
@@ -21,36 +18,34 @@ function disposePythonTerminal() {
         curTerm.dispose();
     }
 }
-
 // Finding proper Python path based on OS
-function getPythonPath(): String {
+function getPythonPath() {
     // TODO: Windows has a lot of wonky python stuff, deal with it
     return "python3";
 }
-
-export function activate(context: vscode.ExtensionContext) {
+function activate(context) {
     // Create an interactive python environment with the current file
     let disposable = vscode.commands.registerCommand('extension.createREPL', () => {
         // If a terminal exists, clean it up
-        disposePythonTerminal()
-
+        disposePythonTerminal();
         // Setup a new terminal with an interactive python environment
         const newTerm = vscode.window.createTerminal('Python');
-        const filePath = vscode.window.activeTextEditor!.document.fileName;
+        const filePath = vscode.window.activeTextEditor.document.fileName;
         const pythonPath = getPythonPath();
-
         if (filePath) {
             newTerm.sendText(`${pythonPath} -i ${filePath}`);
             newTerm.show(true);
-        } else {
+        }
+        else {
             vscode.window.showInformationMessage('No files to run!');
         }
     });
-
     context.subscriptions.push(disposable);
 }
-
-export function deactivate() {
+exports.activate = activate;
+function deactivate() {
     // Clean up terminal
     disposePythonTerminal();
 }
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
