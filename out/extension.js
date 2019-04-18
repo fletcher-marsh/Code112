f82154f880891671749ebe141da7245af22098ef
Fletcher Marsh <fletcher.r.marsh@gmail.com>
2019-04-18 01:41:07 -0400

    first pass at executable checking

--- out/extension.js
+++ out/extension.js
@@ -1,6 +1,8 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
 const vscode = require("vscode");
+const os = require('os');
+const fs = require('fs');
 // Get current Python REPL (if exists)
 function getInteractivePythonTerminal() {
     const allTerms = vscode.window.terminals;
@@ -24,6 +26,43 @@ function getPythonPath() {
     // TODO: this will be replaced by chooseExecutable, but needs starter default val
     return "python3";
 }
+// Checks if any of the input paths are valid executables, and if so append to output
+function getExecsByPaths(paths) {
+    var result = [];
+    paths.forEach(path => {
+        fs.access(path, fs.constants.X_OK, (err) => {
+            err ? null : result.push(path);
+        });
+    });
+    return result;
+}
+// Retrieves all Python executables
+function findAllExecs() {
+    const platform = os.platform();
+    var placesToLook;
+    if (platform === "darwin") {
+        placesToLook = [
+            '/Library/Frameworks/Python.framework/Versions/3.6/bin/python',
+            '/usr/bin/python',
+            '/usr/local/bin/python',
+            '/usr/bin/python',
+            '~/anaconda3/python'
+        ];
+    }
+    else if (platform === "win32" || platform === "win64") {
+        placesToLook = [
+            'C:\ProgramData\Anaconda3\python.exe',
+            'C:\Python36-32\python.exe',
+            'C:\Python37-32\python.exe',
+            'C:\Miniconda3\python.exe',
+            'C:\Users\Fletch\Miniconda3\python.exe'
+        ];
+    }
+    else {
+        placesToLook = [''];
+    }
+    return getExecsByPaths(placesToLook);
+}
 // Set up functionality of Code112
 function activate(context) {
     // Create an interactive python environment with the current file
@@ -45,7 +84,8 @@ function activate(context) {
     // Allow user to select which interpreter/compiler to run against
     let chooseExecutable = vscode.commands.registerCommand('extension.chooseExecutable', () => {
         // TODO: find all interpreters/etc. accessible
-        console.log("Choose executable");
+        const execs = findAllExecs();
+        console.log(os.platform());
         // TODO: update stored value to be read from createREPL
     });
     context.subscriptions.push(createREPL);
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,7 @@
 'use strict';
 import * as vscode from 'vscode';
+const os = require('os');
+const fs = require('fs');
 
 // Get current Python REPL (if exists)
 function getInteractivePythonTerminal(): vscode.Terminal | null {
@@ -29,6 +31,47 @@ function getPythonPath(): String {
     return "python3";
 }
 
+// Checks if any of the input paths are valid executables, and if so append to output
+function getExecsByPaths(paths: String[]): String[] {
+    var result: String[] = [];
+
+    paths.forEach(path => {
+        fs.access(path, fs.constants.X_OK, (err: String) => {
+            err ? null : result.push(path);
+        });
+    });
+    
+    return result;
+}
+
+// Retrieves all Python executables
+function findAllExecs(): String[] {
+    const platform = os.platform();
+    var placesToLook;
+
+    if (platform === "darwin") {
+        placesToLook = [
+            '/Library/Frameworks/Python.framework/Versions/3.6/bin/python',
+            '/usr/bin/python',
+            '/usr/local/bin/python',
+            '/usr/bin/python',
+            '~/anaconda3/python'
+        ];
+    } else if (platform === "win32" || platform === "win64") {
+        placesToLook = [
+            'C:\ProgramData\Anaconda3\python.exe',
+            'C:\Python36-32\python.exe',
+            'C:\Python37-32\python.exe',
+            'C:\Miniconda3\python.exe',
+            'C:\Users\Fletch\Miniconda3\python.exe'
+        ];
+    } else {
+        placesToLook = [''];
+    }
+
+    return getExecsByPaths(placesToLook);
+}
+
 // Set up functionality of Code112
 export function activate(context: vscode.ExtensionContext) {
     // Create an interactive python environment with the current file
@@ -52,7 +95,8 @@ export function activate(context: vscode.ExtensionContext) {
     // Allow user to select which interpreter/compiler to run against
     let chooseExecutable = vscode.commands.registerCommand('extension.chooseExecutable', () => {
         // TODO: find all interpreters/etc. accessible
-        console.log("Choose executable");
+        const execs = findAllExecs();
+        console.log(os.platform());
 
         // TODO: update stored value to be read from createREPL
     });
